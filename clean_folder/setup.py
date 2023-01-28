9d23ebff50443b7bdbcd09d62106ad4dfa31932e
Marinakrash <66419111+marinakrash@users.noreply.github.com>
2023-01-28 11:23:49 +0200

    Add files via upload

---
+++ clean_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='clean_folder',
+      version='1',
+      description='Very useful code to clean your folders',
+      url='https://github.com/marinakrash/sort-files-hw6',
+      author='Marina Krasheninnikova',
+      author_email='marinakrash@example.com',
+      license='MIT',
+      packages=packages=find_namespace_packages()
+)
